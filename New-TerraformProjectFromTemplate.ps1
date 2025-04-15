1e3c659c61b3edca43bc481e6f740b1a850fed14
Dubovsky-Andrey <120334571+Dubovsky-Andrey@users.noreply.github.com>
2025-04-15 15:55:52 -0400

    change var

--- New-TerraformProjectFromTemplate.ps1
+++ New-TerraformProjectFromTemplate.ps1
@@ -29,4 +29,4 @@ function New-TerraformProjectFromTemplate {
 }
 
 # Example usage:
-New-TerraformProjectFromTemplate -ProjectName "aws-ec2-simple"
+New-TerraformProjectFromTemplate -ProjectName "my-project"
